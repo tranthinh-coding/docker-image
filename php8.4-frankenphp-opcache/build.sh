642f4db39601fa9e4d75f49cfe1050ed8a4c904a
Think <tranthinh.own@gmail.com>
2025-12-19 00:57:46 +0700

    feat

--- php8.4-frankenphp-opcache/build.sh
+++ php8.4-frankenphp-opcache/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export APP_ENV="production"
 docker image rm thinktran/php:php8.4-frankenphp-opcache -f
-docker build -t thinktran/php:php8.4-frankenphp-opcache -f Dockerfile .
+docker build --no-cache -t thinktran/php:php8.4-frankenphp-opcache -f Dockerfile .
 docker push thinktran/php:php8.4-frankenphp-opcache
--- php8.4-frankenphp/build.sh
+++ php8.4-frankenphp/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export APP_ENV="production"
 docker image rm thinktran/php:php8.4-frankenphp -f
-docker build -t thinktran/php:php8.4-frankenphp -f Dockerfile .
+docker build --no-cache -t thinktran/php:php8.4-frankenphp -f Dockerfile .
 docker push thinktran/php:php8.4-frankenphp
